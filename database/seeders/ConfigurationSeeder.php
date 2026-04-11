b22415d679b19fc33f4ae89f6731cce666f5774d
hardik prajapati <hardikprajapati8104@gmail.com>
2026-04-11 13:21:11 +0530

    configuration seeeder created

---
+++ database/seeders/ConfigurationSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Configuration;
+use Illuminate\Database\Seeder;
+
+class ConfigurationSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $configurations = [
+            ['key' => 'CONTACT_EMAIL',   'value' => 'support@wealthora.io',    'display_name' => 'Contact Email'], 
+            ['key' => 'CONTACT_PHONE',   'value' => '-',     'display_name' => 'Contact Phone'], 
+            ['key' => 'CONTACT_ADDRESS', 'value' => '-', 'display_name' => 'Contact Address'],
+            ['key' => 'SOCIAL_FACEBOOK_LINK', 'value' => '-', 'display_name' => 'Facebook'],
+            ['key' => 'SOCIAL_TWITTER_LINK', 'value' => '-', 'display_name' => 'Twitter'],
+            ['key' => 'SOCIAL_LINKEDIN_LINK', 'value' => '-', 'display_name' => 'LinkedIn'],
+            ['key' => 'SOCIAL_INSTAGRAM_LINK', 'value' => '-', 'display_name' => 'Instagram'],
+            ['key' => 'SOCIAL_PINTEREST_LINK', 'value' => '-', 'display_name' => 'Pinterest'],
+            ['key' => 'SOCIAL_YOUTUBE_LINK', 'value' => '-', 'display_name' => 'YouTube'],
+            ['key' => 'SOCIAL_WHATSAPP_LINK', 'value' => '-', 'display_name' => 'WhatsApp'],
+            ['key' => 'FOOTER_COPYRIGHT', 'value' => '-', 'display_name' => 'Footer Copyright']
+
+        ];
+
+        foreach ($configurations as $configuration) {
+            Configuration::firstOrCreate(['key' => $configuration['key']], $configuration);
+        }
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
             MenuSeeder::class,
             UserSeeder::class,
             PermissionSeeder::class,
+            ConfigurationSeeder::class,            
         ]);
     }
 }
