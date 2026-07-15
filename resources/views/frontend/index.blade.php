ffd9d46a9235d707f754f52c0b288a4335b8f4ef
GKDevotion <gk@devotiontech.io>
2026-07-15 18:18:27 +0530

    Change weekly to monthly

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1224,7 +1224,7 @@
                     <div class="section-title section-title-center">
                         <h3 class="wow fadeInUp">subscription plans</h3>
                         <h2 class="wow fadeInUp" data-wow-delay="0.2s" data-cursor="-opaque">
-                            Choose Your Plan, Start <span>Profiting</span>
+                            Your Success Starts with the <span class="color-change">Right Plan</span>
                         </h2>
                     </div>
                     <!-- Section Title End -->
