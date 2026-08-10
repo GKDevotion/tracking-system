374931e11be20bae3fd31c89305c7bc8ee1f3cde
GKDevotion <gk@devotiontech.io>
2026-08-10 18:40:49 +0530

    Update TG Name

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -162,7 +162,7 @@ class HomeController extends Controller
 
             try {
                 $ids = TelegramScraper::scrapeDeleted(
-                    '+VRkTH1K9l5cxYzQ8',//'Wealthoraofficial',
+                    'Wealthoraofficial',
                     $post_id
                 );
 
