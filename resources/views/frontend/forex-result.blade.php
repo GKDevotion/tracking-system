7f11ba017a242f5b72595d8a7080603c91745684
GKDevotion <gk@devotiontech.io>
2026-08-19 19:57:15 +0530

    update Track Signal Function

--- resources/views/frontend/forex-result.blade.php
+++ resources/views/frontend/forex-result.blade.php
@@ -233,13 +233,15 @@
                         <tbody>
                                 @forelse($signals as $signal)
                                     <?php
+                                    $res = 0;
                                     $live_btn_url = $signal->live_btn_url;
 
                                     if( $signal->profit != null && $signal->result_id ){
+                                        $res = $signal->result_id;
                                         $live_btn_url = preg_replace('/\/[^\/]+$/', '/' . $signal->result_id, $signal->live_btn_url);
                                     }
                                     ?>
-                                    <tr class="{{ $signal->profit == 0 ? 'row-faded' : '' }} redirect-tg-channel" data-link-url="{{ $live_btn_url }}">
+                                    <tr class="{{ $signal->profit == 0 ? 'row-faded' : '' }} redirect-tg-channel {{$res}}" data-link-url="{{ $live_btn_url }}">
 
                                         {{-- Signal Date --}}
                                         <td class="c-date">
